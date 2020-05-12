9213952d0cdf3915bea4a1c6e11cd868f22ca921
dan <dan.taylor@observian.com>
2020-05-12 08:33:58 -0600

    adding some comments

--- AwsSecretReader/IEnvironmentVariableReader.cs
+++ AwsSecretReader/IEnvironmentVariableReader.cs
@@ -1,5 +1,8 @@
 namespace AwsSecretReader
 {
+	/// <summary>
+	/// Just an interface to wrap reading env variables for unit testing purposes
+	/// </summary>
 	public interface IEnvironmentVariableReader
 	{
 		string GetValue(string key);
